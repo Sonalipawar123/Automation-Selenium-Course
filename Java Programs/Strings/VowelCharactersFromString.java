af566c1356c7be5ad3834982b71aa1b26c98c27e
Sonali Sadashiv Pawar <94914756+Sonalipawar123@users.noreply.github.com>
2021-12-04 15:44:15 +0530

    Update VowelCharactersFromString.java

--- Java Programs/Strings/VowelCharactersFromString.java
+++ Java Programs/Strings/VowelCharactersFromString.java
@@ -5,9 +5,23 @@ public class VowelCharactersFromString {
 	public static void main(String[] args) 
 	{
 
-        String str = new String("Selenium");
-        String str1 = new String("Selenium");
-
+       String s=new String();
+		 Scanner sc=new Scanner(System.in);
+		 System.out.println("enter the string");
+		 s=sc.nextLine();
+		 int vowel = 0;
+      for(int i=0;i<s.length();i++)
+	{
+	     char ch=s.charAt(i);
+	     if(ch!=' ')
+	     {
+	     
+	    if((ch=='a')|| (ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+	   
+	       vowel++;
+		}
+	     System.out.println("vowels="+vowel);
+	
 	}
 
 }
