dd30f27fb50579272c10cf27b61c42b6d69a8f13
Sonali Sadashiv Pawar <94914756+Sonalipawar123@users.noreply.github.com>
2021-12-04 15:41:35 +0530

    Add files via upload

---
+++ Java Programs/Strings/SimpleStringWithLiteralWay.java
@@ -0,0 +1,14 @@
+package StringProgramWithMethods;
+
+public class SimpleStringWithLiteralWay {
+
+	public static void main(String[] args)
+	{
+        
+		String s = "core java";
+		String s2= "Core Java";
+		System.out.println(s);
+		System.out.println(s2);
+	}
+
+}
